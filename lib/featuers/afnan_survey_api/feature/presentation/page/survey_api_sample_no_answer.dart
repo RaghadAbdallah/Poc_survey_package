95486371e8e04f2bb7001dd68c4bf34887ea7abb
rali <eng.raghad94@gmail.com>
2024-04-15 16:24:11 +0300

    build answer design

--- lib/featuers/afnan_survey_api/feature/presentation/page/survey_api_sample_no_answer.dart
+++ lib/featuers/afnan_survey_api/feature/presentation/page/survey_api_sample_no_answer.dart
@@ -3,15 +3,21 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:poc_itg_survey/featuers/afnan_survey_api/feature/presentation/page/question_body.dart';
 import '../cubit/survey_cubit.dart';
 
-class SurveyApiSample extends StatelessWidget {
+class SurveyApiSample extends StatefulWidget {
 
+
+  SurveyApiSample({super.key});
+
+  @override
+  State<SurveyApiSample> createState() => _SurveyApiSampleState();
+}
+
+class _SurveyApiSampleState extends State<SurveyApiSample> {
   final PageController boardController = PageController();
 
   final int indexItem = 0;
-
+  int progressValue = 0;
   List<Widget> listOfQuestions = [];
-
-  SurveyApiSample({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -22,50 +28,90 @@ class SurveyApiSample extends StatelessWidget {
         body: BlocBuilder<NewSurveyCubit, NewSurveyState>(
             builder: (BuildContext context, NewSurveyState state) {
           if (state is NewSurveyStateLoaded) {
-            return SizedBox(
-              child: PageView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                controller: boardController,
-                onPageChanged: (int index) {},
-                itemBuilder: (BuildContext context, int index) {
-                  listOfQuestions = context
-                      .read<NewSurveyCubit>()
-                      .newSurveyDat!
-                      .steps
-                      .map((questionChoice) {
-                    return QuestionBody(
-                      questionIndex: index,
-                      questionType: questionChoice.type == 'question'
-                          ? questionChoice.answerFormat?.type ?? ''
-                          : questionChoice.type,
-                      nextFunction: () async {
-                        boardController.nextPage(
-                            duration: const Duration(milliseconds: 750),
-                            curve: Curves.fastLinearToSlowEaseIn);
+            return Column(
+              children: [
+                SizedBox(
+                  height: 550,
+                  child: PageView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    controller: boardController,
+                    onPageChanged: (int index) {
+                      setState(() {
+                        progressValue=progressValue+1;
+                      });
+                    },
+                    itemBuilder: (BuildContext context, int index) {
+                      listOfQuestions = context
+                          .read<NewSurveyCubit>()
+                          .newSurveyDat!
+                          .steps
+                          .map((questionChoice) {
+                        return QuestionBody(
+                          questionIndex: index,
+                          questionType: questionChoice.type == 'question'
+                              ? questionChoice.answerFormat?.type ?? ''
+                              : questionChoice.type,
+                          nextFunction: () async {
+                            boardController.nextPage(
+                                duration: const Duration(milliseconds: 750),
+                                curve: Curves.fastLinearToSlowEaseIn);
+                          },
+                          previousFunction: () async {
+                            boardController.previousPage(
+                                duration: const Duration(milliseconds: 750),
+                                curve: Curves.fastLinearToSlowEaseIn);
+                          },
+                          surveyModel: questionChoice,
+                          isFinal: context
+                                          .read<NewSurveyCubit>()
+                                          .newSurveyDat!
+                                          .steps
+                                          .length -
+                                      1 ==
+                                  index
+                              ? true
+                              : false,
+                          answerStatues: questionChoice.answerStatus ?? 0,
+                        );
+                      }).toList();
+                      return listOfQuestions[index];
+                    },
+                    itemCount:
+                        context.read<NewSurveyCubit>().newSurveyDat!.steps.length,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    SizedBox(
+                      width: 270,
+                      child: LinearProgressIndicator(
+                        value: progressValue /
+                            context
+                                .read<NewSurveyCubit>()
+                                .newSurveyDat!
+                                .steps
+                                .length,
+                        backgroundColor: Colors.grey,
+                        valueColor:
+                        const AlwaysStoppedAnimation<Color>(Colors.teal),
+                      ),
+                    ),
+                    TextButton(
+                      child: Text(
+                        context.read<Map<String, String>?>()?['close'] ??
+                            'Close',
+                        style: const TextStyle(
+                          color: Colors.teal,
+                        ),
+                      ),
+                      onPressed: () {
+                        // surveyController.closeSurvey(context);
                       },
-                      previousFunction: () async {
-                        boardController.previousPage(
-                            duration: const Duration(milliseconds: 750),
-                            curve: Curves.fastLinearToSlowEaseIn);
-                      },
-                      surveyModel: questionChoice,
-                      isFinal: context
-                                      .read<NewSurveyCubit>()
-                                      .newSurveyDat!
-                                      .steps
-                                      .length -
-                                  1 ==
-                              index
-                          ? true
-                          : false,
-                      answerStatues: questionChoice.answerStatus ?? 0,
-                    );
-                  }).toList();
-                  return listOfQuestions[index];
-                },
-                itemCount:
-                    context.read<NewSurveyCubit>().newSurveyDat!.steps.length,
-              ),
+                    ),
+                  ],
+                )
+              ],
             );
           }
           return Container();
