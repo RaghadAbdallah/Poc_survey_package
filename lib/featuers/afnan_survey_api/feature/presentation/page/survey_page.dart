7916e71d8010785aa224056a6a488d5dc5281b79
aabusaleem <aabusaleem>
2024-04-15 10:33:02 +0300

    build questions design

--- lib/featuers/afnan_survey_api/feature/presentation/page/survey_page.dart
+++ lib/featuers/afnan_survey_api/feature/presentation/page/survey_page.dart
@@ -1,11 +1,11 @@
 
 import 'package:flutter/material.dart';
-import 'package:poc_itg_survey/featuers/afnan_survey_api/feature/presentation/page/survey_screen.dart';
+import 'package:poc_itg_survey/featuers/afnan_survey_api/feature/presentation/page/survey_api_sample_no_answer.dart';
 
-import 'ddd.dart';
+import 'survey_api_sample_answer.dart';
 
-class ddd extends StatelessWidget {
-  const ddd({Key? key}) : super(key: key);
+class SurveyPage extends StatelessWidget {
+  const SurveyPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +18,7 @@ class ddd extends StatelessWidget {
             children: [
               InkWell(
                   onTap: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>SurveyApiSample2()));
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>SurveyApiSampleAnswer()));
                   },
                   child: Text("Answer")),
 
